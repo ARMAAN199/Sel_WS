c730e855bc903bf139d1ad1b0aaf0686196ce267
ARMAAN199 <arman0432.cse19@chitkara.edu.in>
2022-05-06 12:54:05 +0530

    seperated paths

---
+++ config.py
@@ -0,0 +1,3 @@
+tradegroupPath = ["/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[", "]/div[1]/span"]
+tradesPath = ["/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[","]/div[2]"]
+tradeNamePath = ["/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[" , "]/div[1]/div[2]/span" , "]/div[2]/div["]
