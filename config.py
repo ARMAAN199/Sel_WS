23f94943b25fe582cd9016fcb736787f8a48e32a
ARMAAN199 <arman0432.cse19@chitkara.edu.in>
2022-05-11 10:47:16 +0530

    updated config

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
 tradegroupPath = ["/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[", "]/div[1]/span"]
-tradesPath = ["/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[","]/div[2]"]
-tradeNamePath = ["/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[" , "]/div[1]/div[2]/span" , "]/div[2]/div["]
-buyOrderWindowPath = ["/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[" , "]/div[2]/div[1]/div/i", "]/div[2]/div["]
+tradesPath = ["/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[", "]/div[2]"]
+tradeNamePath = ["/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[", "]/div[1]/div[2]/span",
+                 "]/div[2]/div["]
+buyOrderWindowPath = ["/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[", "]/div[2]/div[1]/div/i",
+                      "]/div[2]/div["]
 orderwindow = ["/html/body/div[2]/div/div/div/div/div[2]/div[2]/div"]
 orderwindow_title = ["/html/body/div[2]/div/div/div/div/div[2]/div[2]/div/div[1]/div[1]"]
 delta = ["/html/body/div[2]/div/div/div/div/div[2]/div[2]/div/div[2]/div/div[1]/div[1]/div[2]"]
 inputs = ["/html/body/div[2]/div/div/div/div/div[2]/div[2]/div/div[2]/div/div[2]/div[1]/div[1]/div/div/input",
           "/html/body/div[2]/div/div/div/div/div[2]/div[2]/div/div[2]/div/div[2]/div[1]/div[2]/div/div/input",
           "/html/body/div[2]/div/div/div/div/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/div[1]/div/div/input",
-          "",
-          "",
-          "",
-          "/html/body/div[2]/div[2]/div/div/div/div[2]/div[2]/div/div[2]/div/div[5]/button/span"]
+          "ab",
+          "ab",
+          "ab",
+          "/html/body/div[2]/div/div/div/div/div[2]/div[2]/div/div[2]/div/div[5]/button"]
+msgcomp = ["/html/body/div[1]/div[2]/div/div/div/div/div[2]/span/span"]
