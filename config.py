e21035a226d5012504167a0e386e725bbe0d2b92
ARMAAN199 <arman0432.cse19@chitkara.edu.in>
2022-05-12 12:58:02 +0530

    config update

--- config.py
+++ config.py
@@ -13,5 +13,17 @@ inputs = ["/html/body/div[2]/div/div/div/div/div[2]/div[2]/div/div[2]/div/div[2]
           "ab",
           "ab",
           "ab",
-          "/html/body/div[2]/div/div/div/div/div[2]/div[2]/div/div[2]/div/div[5]/button"]
+          "/html/body/div[2]/div/div/div/div/div[2]/div[2]/div/div[2]/div/div[5]/button/span"]
 msgcomp = ["/html/body/div[1]/div[2]/div/div/div/div/div[2]/span/span"]
+
+edit_price = ["/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[", "]/div[2]/div[", "]/div[2]/div[3]/div/div/div/input"]
+edit_qty = ["/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[", "]/div[2]/div[", "]/div[2]/div[4]/div/div/div/input"]
+edit_btn = ["/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[", "]/div[2]/div[", "]/div[2]/div[7]/button"]
+
+#                                                                  group         trade
+# "/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[3]/div[2]/div/div[2]/div[1]/div/i"
+# "/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[3]/div[2]/div/div[2]/div[3]/div/div/div/input"
+# "/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[3]/div[2]/div/div[2]/div[4]/div/div/div/input"
+# "/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[3]/div[2]/div/div[2]/div[7]/button/span/span/svg"
+# "/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[3]/div[2]/div/div[2]/div[7]/button"
+# "/html/body/div[1]/div[2]/div/div/div/div/div[2]/span/span"
