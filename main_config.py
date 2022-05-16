1c2a9dec42690d1565ec9e6a5daaaecc15474ab4
ARMAAN199 <arman0432.cse19@chitkara.edu.in>
2022-05-16 16:06:37 +0530

    new conf

--- main_config.py
+++ main_config.py
@@ -2,3 +2,12 @@
 #             "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "s", "r", "t"]
 freq_sym = ["BANKNIFTY", "NIFTY"]
 name_for_new_dropdown = ["/html/body/div[1]/div[1]/div[2]/div[2]/div/div[2]/div/div[3]/div[1]/div/div[1]/div/div/div[1]/div[1]"]
+
+
+
+
+# For testing only, ignore.
+# /html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[1]/div[1]/span  premod
+# /html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[2]/div[1]/span new and postmod
+
+#  ttl unhinged
