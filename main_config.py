9e07b4dd074560e8e1dcf923eff78a2da7e098b5
ARMAAN199 <arman0432.cse19@chitkara.edu.in>
2022-05-16 10:37:57 +0530

    new config pertaining to flow

--- main_config.py
+++ main_config.py
@@ -1,2 +1,4 @@
-freq_sym = ["BANKNIFTY", "NIFTY", "acc", "reliance", "aartiind", "adanient", "adaniports", "apollotyre", "axisbank", "dabur", "icicibank",
-            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "s", "r", "t"]
+# freq_sym = ["BANKNIFTY", "NIFTY", "acc", "reliance", "aartiind", "adanient", "adaniports", "apollotyre", "axisbank", "dabur", "icicibank",
+#             "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "s", "r", "t"]
+freq_sym = ["BANKNIFTY", "NIFTY"]
+name_for_new_dropdown = ["/html/body/div[1]/div[1]/div[2]/div[2]/div/div[2]/div/div[3]/div[1]/div/div[1]/div/div/div[1]/div[1]"]
